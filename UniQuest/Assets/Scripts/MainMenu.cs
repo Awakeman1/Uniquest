1be2693608d7c4842f0b37204b4c126accb05e52
MattShell <48376689+MattShell@users.noreply.github.com>
2020-10-23 19:07:34 +1100

    Refactored MainMenu.cs
    Refactored script to make it clearer and less brittle

--- UniQuest/Assets/Scripts/MainMenu.cs
+++ UniQuest/Assets/Scripts/MainMenu.cs
@@ -10,34 +10,59 @@ public class MainMenu : MonoBehaviour
     public static GameObject mmCanvas;
     public static int NumPlayers = 2;
 
+    private GameObject mmBackgroundImg, mmBackgroundBlank;
+    private GameObject menuMain, menuNewGame, menuOptions, menuAddQuestion, menuRmQuestion;
+    private GameObject txtNumPlayers;
 
 
     public void Start()
     {
-        mmCanvas = GameObject.Find("Canvas");
         //Debug.Log("Start");
         Debug.Log("Start " + GetInstanceID(), this);
+
+        mmCanvas = GameObject.Find("Canvas");
+        mmBackgroundImg = GameObject.Find("Background");
+        mmBackgroundBlank = GameObject.Find("Background_Nopic");
+
+        menuMain = GameObject.Find("MainMenu");
+        menuNewGame = GameObject.Find("NGMenu");
+        menuOptions = GameObject.Find("OptionsMenu");
+        menuAddQuestion = GameObject.Find("AddQuestionMenu");
+        menuRmQuestion = GameObject.Find("RmQuestionMenu");
+
+        txtNumPlayers = GameObject.Find("txt_playernum");
+
+        ClearScreen();
+        mmBackgroundImg.gameObject.SetActive(true);
+        menuMain.gameObject.SetActive(true);
+        
+        /*
         mmCanvas.transform.GetChild(0).gameObject.SetActive(true);
         mmCanvas.transform.GetChild(1).gameObject.SetActive(false);
-
         mmCanvas.transform.GetChild(2).gameObject.SetActive(true);
         mmCanvas.transform.GetChild(3).gameObject.SetActive(false); 
         mmCanvas.transform.GetChild(4).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(5).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(6).gameObject.SetActive(false);
+        */
     }
        
   public void OptionMenu()
     {
         Debug.Log("Options");
+
+        ClearScreen();
+        mmBackgroundImg.gameObject.SetActive(true);
+        menuOptions.gameObject.SetActive(true);
+        /*
         mmCanvas.transform.GetChild(0).gameObject.SetActive(true);
         mmCanvas.transform.GetChild(1).gameObject.SetActive(false);
-
         mmCanvas.transform.GetChild(2).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(3).gameObject.SetActive(false); 
         mmCanvas.transform.GetChild(4).gameObject.SetActive(true);
         mmCanvas.transform.GetChild(5).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(6).gameObject.SetActive(false);
+        */
     }
 
 
@@ -45,21 +70,28 @@ public class MainMenu : MonoBehaviour
     public void NewGame ()
     {
         Debug.Log("Newgame");
+
+        ClearScreen();
+        mmBackgroundImg.gameObject.SetActive(true);
+        menuNewGame.gameObject.SetActive(true);
+
+        /*
         mmCanvas.transform.GetChild(0).gameObject.SetActive(true);
         mmCanvas.transform.GetChild(1).gameObject.SetActive(false);
-
         mmCanvas.transform.GetChild(2).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(3).gameObject.SetActive(true); 
         mmCanvas.transform.GetChild(4).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(5).gameObject.SetActive(false);
-        mmCanvas.transform.GetChild(6).gameObject.SetActive(false);;  
+        mmCanvas.transform.GetChild(6).gameObject.SetActive(false);
+        */
     }
 
     public void playercountplus()
     {
         if(NumPlayers < 10){
             NumPlayers++;
-            mmCanvas.transform.GetChild(3).GetChild(5).gameObject.GetComponent<UnityEngine.UI.Text>().text = NumPlayers.ToString();
+            txtNumPlayers.gameObject.GetComponent<UnityEngine.UI.Text>().text = NumPlayers.ToString();
+            //mmCanvas.transform.GetChild(3).GetChild(5).gameObject.GetComponent<UnityEngine.UI.Text>().text = NumPlayers.ToString();
         }
     }
 
@@ -67,7 +99,8 @@ public class MainMenu : MonoBehaviour
     {
         if(NumPlayers > 2){
             NumPlayers--;
-            mmCanvas.transform.GetChild(3).GetChild(5).gameObject.GetComponent<UnityEngine.UI.Text>().text = NumPlayers.ToString();
+            txtNumPlayers.gameObject.GetComponent<UnityEngine.UI.Text>().text = NumPlayers.ToString();
+            //mmCanvas.transform.GetChild(3).GetChild(5).gameObject.GetComponent<UnityEngine.UI.Text>().text = NumPlayers.ToString();
         }
     }
 
@@ -86,6 +119,11 @@ public class MainMenu : MonoBehaviour
     public void Back()
     {
         Debug.Log("Back");
+        ClearScreen();
+        mmBackgroundImg.gameObject.SetActive(true);
+        menuMain.gameObject.SetActive(true);
+
+        /*
         mmCanvas.transform.GetChild(0).gameObject.SetActive(true);
         mmCanvas.transform.GetChild(1).gameObject.SetActive(false);
 
@@ -94,32 +132,57 @@ public class MainMenu : MonoBehaviour
         mmCanvas.transform.GetChild(4).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(5).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(6).gameObject.SetActive(false);
+        */
     }
 
     public void AddQuestion()
     {
         Debug.Log("Add Qn");
+
+        ClearScreen();
+        mmBackgroundBlank.gameObject.SetActive(true);
+        menuAddQuestion.gameObject.SetActive(true);
+
+
+        /*
         mmCanvas.transform.GetChild(0).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(1).gameObject.SetActive(true);
-
         mmCanvas.transform.GetChild(2).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(3).gameObject.SetActive(false); 
         mmCanvas.transform.GetChild(4).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(5).gameObject.SetActive(true);
         mmCanvas.transform.GetChild(6).gameObject.SetActive(false);
+        */
     }
 
     public void RMQuestion()
     {
         Debug.Log("Rm Question");
+
+        ClearScreen();
+        mmBackgroundBlank.gameObject.SetActive(true);
+        menuRmQuestion.gameObject.SetActive(true);
+
+        /*
         mmCanvas.transform.GetChild(0).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(1).gameObject.SetActive(true);
-
         mmCanvas.transform.GetChild(2).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(3).gameObject.SetActive(false); 
         mmCanvas.transform.GetChild(4).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(5).gameObject.SetActive(false);
         mmCanvas.transform.GetChild(6).gameObject.SetActive(true);
+        */
+    }
+
+    private void ClearScreen()
+    {
+        mmBackgroundImg.gameObject.SetActive(false);
+        mmBackgroundBlank.gameObject.SetActive(false);
+        menuMain.gameObject.SetActive(false);
+        menuNewGame.gameObject.SetActive(false);
+        menuOptions.gameObject.SetActive(false);
+        menuAddQuestion.gameObject.SetActive(false);
+        menuRmQuestion.gameObject.SetActive(false);
     }
 
 
